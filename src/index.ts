b3b5aca9b786c016cb02f7508afb9d030a497a26
Prateek Malhotra <prateek@condorcapital.com>
2018-12-12 15:32:30 -0500

    Export useful class an interface for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import IdleTimeout from './IdleTimeout';
 import OptionsInterface from './interfaces/Options';
 
+export { IdleTimeout, OptionsInterface };
+
 export default (callback: () => void, options?: OptionsInterface | undefined) =>
   new IdleTimeout(callback, options);
